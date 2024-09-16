5c4865168a0a02e674897fbae197dc015b5bfeb9
=Julián Sánchez <julisanf@gmail.com>
2024-09-16 19:25:48 +0200

    Adds UsersDatabase class and extends the Fraction implementation with the bare minimum to compile #7

--- src/main/java/es/upm/miw/iwvg_devops/code/Fraction.java
+++ src/main/java/es/upm/miw/iwvg_devops/code/Fraction.java
@@ -4,4 +4,12 @@ package es.upm.miw.iwvg_devops.code;
  * Stub class to be replaced with a proper implementation
  */
 public class Fraction {
+
+    public Fraction(int numerator, int denominator) {
+    }
+
+    public Fraction() {
+        this(0, 0);
+    }
+
 }
